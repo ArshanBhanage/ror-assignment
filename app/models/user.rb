00b98484ad9cbde87dad29287b6e7a97c59ff8a6
Arshan Bhanage <bhanagearshan@gmail.com>
2024-04-12 16:06:03 +0530

    added extra fields to users

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,9 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+  enum role: [:receptionist, :doctor]
+
+  validates :first_name, presence: true
+  validates :last_name, presence: true
+  validates :role, presence: true  
 end
